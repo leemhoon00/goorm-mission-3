c14193c075fa45acc89787db2c499d6fc804443c
leemhoon00 <leemhoon000@gmail.com>
2023-12-08 17:17:24 +0900

    feat: create network

--- main.tf
+++ main.tf
@@ -18,36 +18,9 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "Main"
-  }
-}
-
-resource "aws_subnet" "subnet-1" {
-  availability_zone = "ap-northeast-2a"
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
-
-resource "aws_subnet" "subnet-2" {
-  availability_zone = "ap-northeast-2c"
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
 
 resource "aws_instance" "example" {
   ami           = "ami-01123b84e2a4fba05"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet-1.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.private-a.id
 }

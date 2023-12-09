c4353885c95cba3c7e457c7dbac6afae07cd6dc5
leemhoon00 <leemhoon000@gmail.com>
2023-12-09 22:14:50 +0900

    Add HTTP to HTTPS redirect and configure HTTPS listener

--- main.tf
+++ main.tf
@@ -111,10 +111,27 @@ resource "aws_autoscaling_group" "nginx" {
   vpc_zone_identifier  = [for subnet in module.az-network : subnet.private_subnet_id]
 }
 
-resource "aws_lb_listener" "nginx" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.nginx.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.nginx.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
 
   default_action {
     target_group_arn = aws_lb_target_group.nginx.arn
@@ -132,4 +149,16 @@ resource "aws_lb_target_group" "nginx" {
 resource "aws_autoscaling_attachment" "nginx" {
   autoscaling_group_name = aws_autoscaling_group.nginx.name
   lb_target_group_arn    = aws_lb_target_group.nginx.arn
+}
+
+resource "aws_route53_record" "nginx" {
+  zone_id = var.zone_id
+  name    = "nginx"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.nginx.dns_name
+    zone_id                = aws_lb.nginx.zone_id
+    evaluate_target_health = true
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "certificate_arn" {
+  type = string
+}
+
+variable "zone_id" {
+  type = string
+}
